b2bec05a2e94d9cd454c3c03773d31895f98e0bb
MiKhin1115 <mikhinhantharsoe@uit.edu.mm>
2026-03-01 10:31:54 +0630

    SHit

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+
+const List = () => {
+  return (
+    <div>
+      <Form></Form>
+        <i className='fa fa-user'></i> List
+      <button className='btn btn-success'>Hello Bootstrap</button>
+    </div>
+  )
+}
+
+export default List
