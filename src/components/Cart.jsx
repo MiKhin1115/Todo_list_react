f3e21dbcef6558e68db7b3fc216e51176e3ef43f
MiKhin1115 <mikhinhantharsoe@uit.edu.mm>
2026-03-02 07:34:30 +0630

    Update complete

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Cart = ({tasks, deleteTask}) => {
+const Cart = ({tasks, deleteTask, updateTask}) => {
 
  const handleDeleteTask = (id) => {
   if(window.confirm("Are you sure ?")){
@@ -11,9 +11,9 @@ const Cart = ({tasks, deleteTask}) => {
   return (
     <div>
       {tasks.map((task) => (
-        <div key={task.id} className="list-group-item w-80 mt-3 shadow-sm">
+        <div key={task.id} className={task.complete ? 'list-group-item w-80 mt-3 shadow-sm bg-success text-decoration-line-through text-white' : 'list-group-item w-80 mt-3 shadow-sm'}>
           <div className="row">
-            <div className="col-9 offset-1">{task.tasks}</div>
+            <div className="col-9 offset-1"> <input type="checkbox" className="me-2" onClick={ () => updateTask(task.id, !task.complete)} checked={task.complete}/> {task.tasks}</div>
             <div className="col-2">
               <i className="fa-solid fa-trash" onClick={ () => handleDeleteTask(task.id)}></i>
             </div>
